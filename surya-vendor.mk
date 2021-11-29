46531f67b23d0b28036781b01bbcf4803af042e8
johnmart19 <johnivan19999@gmail.com>
2021-11-29 11:58:48 +0200

    surya: Import VulkanAPI Layers from Google Pixel 4
    Imported from dump flame-user 12 SP1A.211105.002 7743617 release-keys
    
    Change-Id: I6280eb619f1d0218037dfd616e8454bacaa16a07

--- surya-vendor.mk
+++ surya-vendor.mk
@@ -969,7 +969,9 @@ PRODUCT_PACKAGES += \
     CneApp \
     IWlanService \
     TimeService \
+    adreno_graphics_driver \
     com.qualcomm.qti.gpudrivers.sm6150.api30 \
+    gpu_profiling_vulkan_layer \
     PowerOffAlarm \
     QtiTelephonyService \
     WfdService \
